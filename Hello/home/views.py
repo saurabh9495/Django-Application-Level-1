3a61d0bbbfe4dd440f9afa8c4595ed2680305a49
Saurabh Kumar <saurabhkumar4829@gmail.com>
2022-08-14 23:21:25 +0000

    ok

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return HttpResponse("This is homepage")
+
+def about(request):
+    return HttpResponse("This is about")
+
+def services(request):
+    return HttpResponse("This is contact")
