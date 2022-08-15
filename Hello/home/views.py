c6274fd0c175f3cc8dc7b3fa541b2347df75ebef
Saurabh Kumar <saurabhkumar4829@gmail.com>
2022-08-15 20:47:22 +0000

    carosel

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -1,13 +1,32 @@
 from multiprocessing import context
 from django.shortcuts import render, HttpResponse
+import requests
+import random
 # Create your views here.
 
 
 def index(request):
-    context = {
-        'variable1': "Saurabh",
-        'variable2': "Kumar"
+
+    url = "https://api.unsplash.com/search/photos?page=" + \
+        str(random.randint(1, 100)) + \
+        "&query=girl&client_id=1ZIf-xckVpzX5RWlYQ-IUGym-4Q6a3En8JVs3zciX_Y"
+
+    payload = {}
+    headers = {
+        'Cookie': 'ugid=42bbb2992106803b518bbc71da8f1e395535305'
     }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    data = response.json()
+
+    context = {}
+    for key, value in enumerate(data['results']):
+        # print(value['urls']['full'])
+        context["variable"+str(key)] = value['urls']['raw']
+
+    # print(context)
+
     return render(request, 'index.html', context)
     # return HttpResponse("This is homepage")
 
