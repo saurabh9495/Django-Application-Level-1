0e7859364ceb956cab121f924ffc01f2c722712f
Saurabh Kumar <saurabhkumar4829@gmail.com>
2022-08-15 00:21:40 +0000

    bootstrap

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render, HttpResponse
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("This is homepage")
+    context = {
+        'variable1': "Saurabh",
+        'variable2':"Kumar"
+    }
+    return render(request, 'index.html', context)
+    # return HttpResponse("This is homepage")
+
 
 def about(request):
     return HttpResponse("This is about")
 
+
 def services(request):
     return HttpResponse("This is services")
+
 
 def contact(request):
     return HttpResponse("This is contact")
